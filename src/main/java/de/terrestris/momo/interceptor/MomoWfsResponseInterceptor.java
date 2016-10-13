e36e9b32115ec9742c95765c15c55c68571809a0
arieger <rieger@terrestris.de>
2016-10-13 10:44:14 +0200

    added wfs interceptor

---
+++ src/main/java/de/terrestris/momo/interceptor/MomoWfsResponseInterceptor.java
@@ -0,0 +1,41 @@
+package de.terrestris.momo.interceptor;
+
+import de.terrestris.shogun2.util.interceptor.MutableHttpServletRequest;
+import de.terrestris.shogun2.util.interceptor.WfsResponseInterceptorInterface;
+import de.terrestris.shogun2.util.model.Response;
+
+/**
+ * This class demonstrates how to implement the WfsResponseInterceptorInterface.
+ *
+ * @author Daniel Koch
+ * @author terrestris GmbH & Co. KG
+ *
+ */
+public class MomoWfsResponseInterceptor implements WfsResponseInterceptorInterface{
+
+	@Override
+	public Response interceptGetCapabilities(MutableHttpServletRequest request, Response response) {
+		return response;
+	}
+
+	@Override
+	public Response interceptDescribeFeatureType(MutableHttpServletRequest request, Response response) {
+		return response;
+	}
+
+	@Override
+	public Response interceptGetFeature(MutableHttpServletRequest request, Response response) {
+		return response;
+	}
+
+	@Override
+	public Response interceptLockFeature(MutableHttpServletRequest request, Response response) {
+		return response;
+	}
+
+	@Override
+	public Response interceptTransaction(MutableHttpServletRequest request, Response response) {
+		return response;
+	}
+
+}
