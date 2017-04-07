e8a3522f59ba58606324b42d42796eaf3cad3176
Andre Henn <henn@terrestris.de>
2017-04-07 11:38:57 +0200

    adds security util

---
+++ src/main/java/de/terrestris/momo/util/security/MomoSecurityUtil.java
@@ -0,0 +1,116 @@
+package de.terrestris.momo.util.security;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import de.terrestris.momo.model.MomoUser;
+import de.terrestris.momo.util.config.MomoConfigHolder;
+
+/**
+ *
+ * terrestris GmbH & Co. KG
+ * @author Andre Henn
+ * @date 06.04.2017
+ *
+ */
+@Component
+public class MomoSecurityUtil {
+
+	private static MomoConfigHolder configHolder;
+
+	/**
+	 *
+	 * @return
+	 */
+	public static boolean currentUserIsSuperAdmin(){
+		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		final Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		final Object principal = authentication.getPrincipal();
+
+		if (principal instanceof MomoUser) {
+			for (GrantedAuthority authority : authorities) {
+				if(authority.getAuthority().equals(configHolder.getSubAdminRoleName())) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public static boolean currentUserIsSubAdminInAnyGroup(){
+		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		final Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		final Object principal = authentication.getPrincipal();
+
+		if (principal instanceof MomoUser) {
+			for (GrantedAuthority authority : authorities) {
+				if(authority.getAuthority().equals(configHolder.getSubAdminRoleName())) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public static boolean currentUserIsEditorInAnyGroup(){
+		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		final Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		final Object principal = authentication.getPrincipal();
+
+		if (principal instanceof MomoUser) {
+			for (GrantedAuthority authority : authorities) {
+				if(authority.getAuthority().equals(configHolder.getEditorRoleName())) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public static boolean currentUserIsUserInAnyGroup(){
+		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		final Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		final Object principal = authentication.getPrincipal();
+
+		if (principal instanceof MomoUser) {
+			for (GrantedAuthority authority : authorities) {
+				if(authority.getAuthority().equals(configHolder.getDefaultUserRoleName())) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * @param configHolder the configHolder to set
+	 */
+	@Autowired
+	@Qualifier("momoConfigHolder")
+	public static void setConfigHolder(MomoConfigHolder configHolder) {
+		MomoSecurityUtil.configHolder = configHolder;
+	}
+
+}
