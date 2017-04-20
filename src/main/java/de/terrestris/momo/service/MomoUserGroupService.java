b34304db1f267bdc4a4692c92d27532d76e0481a
Johannes Weskamm <weskamm@terrestris.de>
2017-04-20 17:47:40 +0200

    Added overrides in REST Group Controller

--- src/main/java/de/terrestris/momo/service/MomoUserGroupService.java
+++ src/main/java/de/terrestris/momo/service/MomoUserGroupService.java
@@ -2,11 +2,17 @@ package de.terrestris.momo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
+import de.terrestris.momo.dao.MomoUserDao;
 import de.terrestris.momo.dao.MomoUserGroupDao;
+import de.terrestris.momo.model.MomoUser;
 import de.terrestris.momo.model.MomoUserGroup;
+import de.terrestris.shogun2.dao.UserDao;
+import de.terrestris.shogun2.model.User;
 import de.terrestris.shogun2.service.UserGroupService;
+import de.terrestris.shogun2.service.UserService;
 
 /**
  *
@@ -46,5 +52,24 @@ public class MomoUserGroupService<E extends MomoUserGroup, D extends MomoUserGro
 	@Qualifier("momoUserGroupDao")
 	public void setDao(D dao) {
 		this.dao = dao;
+	}
+
+	@Autowired
+	@Qualifier("momoUserService")
+	private MomoUserService<MomoUser, MomoUserDao<MomoUser>> momoUserService;
+
+	/**
+	 * Override in order to set the owner and correct class..
+	 */
+	@Override
+	@PreAuthorize("hasRole(@configHolder.getSuperAdminRoleName())"
+			+ " or (#e.id == null and hasPermission(#e, 'CREATE'))"
+			+ " or (#e.id != null and hasPermission(#e, 'UPDATE'))")
+	public void saveOrUpdate(E e) {
+		MomoUser user = momoUserService.getUserBySession();
+		if (user != null) {
+			e.setOwner(user);
+		}
+		dao.saveOrUpdate(e);
 	}
 }
