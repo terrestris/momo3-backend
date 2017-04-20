f7670f626e95d1869d967995c9fb67c9ed3c5e79
Johannes Weskamm <weskamm@terrestris.de>
2017-04-20 17:47:40 +0200

    Added overrides in REST Group Controller

--- src/main/java/de/terrestris/momo/rest/MomoUserGroupRestController.java
+++ src/main/java/de/terrestris/momo/rest/MomoUserGroupRestController.java
@@ -6,12 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.terrestris.momo.dao.MomoUserGroupDao;
+import de.terrestris.momo.model.MomoUser;
 import de.terrestris.momo.model.MomoUserGroup;
 import de.terrestris.momo.service.MomoUserGroupService;
 import de.terrestris.shogun2.rest.UserGroupRestController;
 
 /**
- * 
+ *
  * @author Daniel Koch
  * @author terrestris GmbH & Co. KG
  *
@@ -23,6 +24,22 @@ import de.terrestris.shogun2.rest.UserGroupRestController;
 @RequestMapping("/momousergroups")
 public class MomoUserGroupRestController<E extends MomoUserGroup, D extends MomoUserGroupDao<E>, S extends MomoUserGroupService<E, D>>
 		extends UserGroupRestController<E, D, S> {
+
+	/**
+	 * Default constructor, which calls the type-constructor
+	 */
+	@SuppressWarnings("unchecked")
+	public MomoUserGroupRestController() {
+		this((Class<E>) MomoUserGroup.class);
+	}
+
+	/**
+	 * Constructor that sets the concrete entity class for the controller.
+	 * Subclasses MUST call this constructor.
+	 */
+	protected MomoUserGroupRestController(Class<E> entityClass) {
+		super(entityClass);
+	}
 
 	/**
 	 * We have to use {@link Qualifier} to define the correct service here.
